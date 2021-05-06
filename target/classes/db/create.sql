fce3f6dfc7daafe0b5a22c3cb60063b924e3da0d
Brian <bweloba@gmail.com>
2021-05-06 16:14:06 +0300

    Prepare for deployment

---
+++ target/classes/db/create.sql
@@ -0,0 +1,28 @@
+SET MODE PostgreSQL;
+
+CREATE DATABASE shopping;
+CREATE TABLE IF NOT EXISTS stores
+(
+    id      int PRIMARY KEY auto_increment,
+    name    VARCHAR,
+    address VARCHAR,
+    website VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS items
+(
+    id      int PRIMARY KEY auto_increment,
+    name    VARCHAR,
+    brand VARCHAR,
+    quantity VARCHAR,
+    price   INTEGER,
+    storeid INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS stores_items
+(
+    id int PRIMARY KEY auto_increment,
+    itemid INTEGER,
+    storeid INTEGER
+)
+
